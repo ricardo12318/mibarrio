abc2ffa1320a30daa4029517573dc9d9f88d7ef4
ricardo12318 <ric16916441@gmail.com>
2022-10-19 12:48:58 -0500

    Create MibarrioApplicationTests.java

---
+++ mibarrio/src/test/java/com/tiendavirtual/mibarrio/MibarrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendavirtual.mibarrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibarrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
